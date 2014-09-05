8ff1d75775044ac38106080fb31ba11f7c314b7c
Fluffy Cutesy Jar-Bear & Elena Sharma <pair+brasschaos.elenasharma@devbootcamp.com>
2014-09-05 11:34:25 -0700

    added View methods * set current player * set cell color * altered column names to be 1..7 * starting to alter snakecase to be camelcase

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,7 @@ $('document').ready(function(){
   var game = new Game(board, player1, player2);
   var controller = new Controller(game, view);
   game.start_game();
-  view.set_current_player(game.current_player); // VIEW METHOOOOOD
+  view.setCurrentPlayer(game.current_player); // VIEW METHOOOOOD
 
 
 });
@@ -22,13 +22,13 @@ var columnClicked = function(ev) {
   var color = this.game.current_player.color
   var row = this.game.drop_piece(column)
   // REMOVE EVENT LISTENER IF ROW == 0
-
-  this.view.set_cell(column, row, color) // VIEW METHOOOOOOD
+  this.view.setCurrentPlayer(game.current_player);
+  this.view.setCellColor(row+1, column, color) // VIEW METHOOOOOOD
 }
 
 Controller.prototype = {
   bindListeners: function(){
-    var columns = this.view.getColumns() // VIEW METHOOOOOD
+    var columns = this.view.getColumns()
     for (i=0; i < columns.length; i++){
       columns[i].addEventListener("click", columnClicked).bind(this)
     }
--- js/view.js
+++ js/view.js
@@ -3,31 +3,26 @@ function View(){
 
 View.prototype = {
 
-  set_current_player: function(player) {
+  setCurrentPlayer: function(player) {
     // will be able to call player.name and player.color on this
+    // $('#current_player').text(player.name)
+    if (player.name == )
   },
 
-  setColor: function(row, col, color) {
-
-  },
-
-  setCell: function(x, y, color) {
-
+  setCellColor: function(row, col, color) {
+    $('#'+col+' .cell:nth-child('+(row)+')').css('background-color', color)
   },
 
   getColumns: function() {
-    // should return all columns using a JQuery selector
+    return $('.column');
   }
 }
 
-$( ".column .cell" ).on("click", function() {
-  $( this ).className( "red_cell" );
-});
+// $( ".column .cell" ).on("click", function() {
+//   $( this ).className( "red_cell" );
+// });
 // $("#1 .cell:first").on("click", console.log("omg"))
 
 //   function set_color(){
 //   $( ".last" ).css("background-color", "red")
 // });
-
-};
-
